e9ae282a376c0a3e0b47590da7c5b0ed73993ad8
kid <nakid001@gmail.com>
2017-02-18 23:44:19 +0700

    Init

---
+++ Advanture of the ancient/Assets/Script/PlayerArmController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArmController : MonoBehaviour {
+    private float t=0;
+    private bool hit = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Hit!");
+            hit = true;
+        //    gameObject.transform.Translate(Vector3.back * Time.deltaTime * 10f);
+
+        }
+        if(hit==true)
+        {
+            t += Time.deltaTime;
+            if(t<=1)
+                gameObject.transform.Translate(Vector3.forward * Time.deltaTime * 1f);
+            if (t >= 1 && t < 2)
+            {
+                gameObject.transform.Translate(Vector3.back * Time.deltaTime * 1f);
+            }
+            if (t >= 2)
+                {
+                    hit = false;
+                    t = 0;
+                }
+         }
+
+
+    }
+}
